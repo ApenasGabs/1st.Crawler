4973cb3ef5ac3f31c3a14d20e464660159d448f2
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-07-05 16:51:36 -0300

    funcao dolar para real concluida

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const puppeteer = require("puppeteer");
 async function robo() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://venturus.org.br/");
-  await page.screenshot({ path: "img/venturus.png" });
+  const moedaBase = "dolar",
+    moedaFinal = "real";
+  const myPage = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}`;
+  await page.goto(myPage);
+  const resultado = await page.evaluate(() => {
+    return document.querySelector(".a61j6.vk_gy.vk_sh.Hg3mWc").value;
+  });
+  console.log(`O valor de 1 ${moedaBase} para ${moedaFinal} é ${resultado}`);
   await browser.close();
 }
-
 robo();
